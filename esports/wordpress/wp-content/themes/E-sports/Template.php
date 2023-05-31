568ef11c0e18c35baabfa422cbafe141c30ff355
Firey-event559 <RaminoHiromiVrca@gmail.com>
2023-05-31 14:48:16 +0200

    template

--- esports/wordpress/wp-content/themes/E-sports/Template.php
+++ esports/wordpress/wp-content/themes/E-sports/Template.php
@@ -1,1 +1,19 @@
 <?php /* Template Name: esports */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php wp_head(); ?>
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    <h1> <?php echo get_the_title(); ?><h1>
+    <p> <?php echo get_the_content(); ?> </p>
+    <?php get_footer(); ?>
+    
+</body>
+</html>
