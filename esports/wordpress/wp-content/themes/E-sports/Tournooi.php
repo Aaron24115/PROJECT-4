256acbae97fdd0efdc9079847b44a9a681b8865b
DonTeuni <9018921@student.zadkine.nl>
2023-06-13 09:20:27 +0200

    Update Tournooi.php

--- esports/wordpress/wp-content/themes/E-sports/Tournooi.php
+++ esports/wordpress/wp-content/themes/E-sports/Tournooi.php
@@ -9,6 +9,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php wp_head(); ?>
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    <h1 class="teamleden-namen"> <?php echo get_the_title(); ?><h1>
+    <p> <?php echo get_the_content(); ?> </p>
+    <?php get_footer(); ?>
 </body>
 </html> 
