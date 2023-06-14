e73efffc2f568664da5cf999354619dcc1facd5b
Firey-event559 <RaminoHiromiVrca@gmail.com>
2023-06-14 16:53:03 +0200

    Code

--- esports/wordpress/wp-content/themes/E-sports/Template.php
+++ esports/wordpress/wp-content/themes/E-sports/Template.php
@@ -12,9 +12,22 @@
 <?php wp_head(); ?>
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
     <h1 class="teamleden-namen"> <?php echo get_the_title(); ?><h1>
-    <p> <?php echo get_the_content(); ?> </p>
-    <h2 class="h2-toernooien"> <?php echo get_the_title(); ?><h2>
-    <?php get_footer(); ?>
+    <p class="Ramino"> <?php echo get_the_content(); ?> </p>
+    <div class="footer"> <?php get_footer(); ?></div>
+
+    <h1 class="leeg"></h1>
+    <footer class="footer3">
+  <a href="https://www.youtube.com" target="_blank">
+<img class="youtube" src="<?php echo get_template_directory_uri(); ?>../img/youtube.jpeg" alt="youtube_logo">
+</a>
+<a href="https://www.instagram.com/vic_iousvultures/" target="_blank">
+<img class="insta" src="<?php echo get_template_directory_uri(); ?>/img/Instagram.png" alt="insta_logo">
+</a>
+<a href="https://www.twitter.com" target="_blank">
+<img class="twitter" src="<?php echo get_template_directory_uri(); ?>/img/twitter.jpg" alt="twitter_logo">
+</a>
+</footer>
+</h1>
 
 </body>
 </html>
