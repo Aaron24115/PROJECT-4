ec37c8ff604ce174d6c0953d78902a7c992fdd22
Firey-event559 <RaminoHiromiVrca@gmail.com>
2023-06-14 09:00:12 +0200

    update over ons pagina

--- esports/wordpress/wp-content/themes/E-sports/contact.php
+++ esports/wordpress/wp-content/themes/E-sports/contact.php
@@ -14,7 +14,25 @@
     <h1 class="contact"> <?php echo get_the_title(); ?><h1>
     <p> <?php echo get_the_content(); ?> </p>
     <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]'); ?>
+<div class="footer">
     <?php get_footer(); ?>
+</div>
+
+
+
+<h1 class="leeg"></h1>
+    <footer class="footer3">
+  <a href="https://www.youtube.com" target="_blank">
+<img class="youtube" src="<?php echo get_template_directory_uri(); ?>../img/youtube.jpeg" alt="youtube_logo">
+</a>
+<a href="https://www.instagram.com/vic_iousvultures/" target="_blank">
+<img class="insta" src="<?php echo get_template_directory_uri(); ?>/img/Instagram.png" alt="insta_logo">
+</a>
+<a href="https://www.twitter.com" target="_blank">
+<img class="twitter" src="<?php echo get_template_directory_uri(); ?>/img/twitter.jpg" alt="twitter_logo">
+</a>
+</footer>
+</h1>
     
 </body>
 </html>
--- esports/wordpress/wp-content/themes/E-sports/over_ons.php
+++ esports/wordpress/wp-content/themes/E-sports/over_ons.php
@@ -11,11 +11,12 @@
 
 <?php wp_head(); ?>
     <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-    <h1 class="contact"> <?php echo get_the_title(); ?><h1>
+    <h1 class="Team"> <?php echo get_the_title(); ?><h1>
     <p> <?php echo get_the_content(); ?> </p>
 
     <div class="footer"><?php get_footer(); ?></div>
 
+   
     <footer class="footer2">
   <a href="https://www.youtube.com" target="_blank">
 <img class="youtube" src="<?php echo get_template_directory_uri(); ?>../img/youtube.jpeg" alt="youtube_logo">
@@ -27,6 +28,7 @@
 <img class="twitter" src="<?php echo get_template_directory_uri(); ?>/img/twitter.jpg" alt="twitter_logo">
 </a>
 </footer>
+
     
 </body>
 </html>
